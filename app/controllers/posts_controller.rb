582b8ec89bb72bf1f1e7e8595604d4ee3b33669c
Manoj_Naidu619 <manojnaidu619@gmail.com>
2018-01-27 21:20:37 +0530

    Implemented admin panel and devise authentication

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,46 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!
+  
+  def index
+    @post = Post.all
+  end
+  def new
+    @post = Post.new
+  end
+  def edit
+    @post = Post.find(params[:id])
+  end
+  def show
+    @post = Post.find(params[:id])
+  end
+  def update
+    @post = Post.find(params[:id])
+     if @post.update_attributes(post_params)
+       redirect_to post_path
+     else
+       render "edit"
+     end
+  end
+  def create
+    @post = Post.new(post_params)
+     if @post.save
+       redirect_to post_path(@post)
+     else
+       render "new"
+     end
+  end
+  def destroy
+    @post = Post.find(params[:id])
+     if @post.delete
+       redirect_to root_path
+     else
+       render "destroy"
+     end
+  end
+
+  private
+
+   def post_params
+     params.require(:post).permit(:title , :content)
+   end
+end
